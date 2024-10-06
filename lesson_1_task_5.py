d1b79166b90b4d38d511a41e4286cb036b5dba35
Savichiha <aabeltyu@gmail.com>
2024-10-06 13:57:17 +0300

    test

---
+++ lesson_1_task_5.py
@@ -0,0 +1,14 @@
+def telefon(num):
+    print(num)
+for _ in range(1):
+    telefon("8")
+    telefon("8")
+    telefon("0")
+    telefon("0")
+    telefon("5")
+    telefon("5")
+    telefon("5")
+    telefon("3")
+    telefon("5")
+    telefon("3")
+    telefon("5")
