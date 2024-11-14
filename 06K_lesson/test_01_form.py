1853fd5987c13d46b4bacfdff17097a0ac414035
Savichiha <aabeltyu@gmail.com>
2024-11-14 15:00:41 +0300

    правки 6 КР

--- 06K_lesson/test_01_form.py
+++ 06K_lesson/test_01_form.py
@@ -4,31 +4,41 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
 @pytest.fixture
 def driver():
     driver = webdriver.Chrome()
     yield driver
     driver.quit()
 
-
 def test_form_submission(driver):
     driver.get("https://bonigarcia.dev/selenium-webdriver-java/data-types.html")
 
-    driver.find_element(By.NAME, "first-name").send_keys("Иван")
-    driver.find_element(By.NAME, "last-name").send_keys("Петров")
-    driver.find_element(By.NAME, "address").send_keys("Ленина, 55-3")
-    driver.find_element(By.NAME, "e-mail").send_keys("test@skypro.com")
-    driver.find_element(By.NAME, "phone").send_keys("+7985899998787")
-    driver.find_element(By.NAME, "city").send_keys("Москва")
-    driver.find_element(By.NAME, "country").send_keys("Россия")
-    driver.find_element(By.NAME, "job-position").send_keys("QA")
-    driver.find_element(By.NAME, "company").send_keys("SkyPro")
+    form_data = {
+        "first-name": "Иван",
+        "last-name": "Петров",
+        "address": "Ленина, 55-3",
+        "e-mail": "test@skypro.com",
+        "phone": "+7985899998787",
+        "city": "Москва",
+        "country": "Россия",
+        "job-position": "QA",
+        "company": "SkyPro"
+    }
+
+    for name in form_data.keys():
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.NAME, name))
+        )
+
+    for name, value in form_data.items():
+        element = driver.find_element(By.NAME, name)
+        element.clear()  # На всякий случай очистим поле перед вводом
+        element.send_keys(value)
 
     driver.find_element(By.TAG_NAME, "button").click()
 
-    WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "zip-code"))
+    WebDriverWait(driver, 20).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "#zip-code"))
     )
 
     zip_field_color = driver.find_element(By.CSS_SELECTOR, "#zip-code").value_of_css_property("background-color")
